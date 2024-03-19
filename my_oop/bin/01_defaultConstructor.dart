062d610004dea9848a5a3c5ab34c6ebd15f11f8b
Marisa Istaltofa <istaltofafa@gmail.com>
2024-03-19 11:41:47 +0700

    01_defaultConstructor

---
+++ my_oop/bin/01_defaultConstructor.dart
@@ -0,0 +1,14 @@
+class Laptop{
+  String? merk;
+  int? harga;
+
+  //Konstruktor
+  Laptop(){
+    print("ini adalah konstruktor default");
+  }
+}
+
+void main() {
+  // disini laptop adalah objek dari kelas Laptop
+  Laptop laptop = Laptop();
+}
