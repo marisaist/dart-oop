dc5eaffd1cddba53f0998cdaae4380a1c2926b37
Marisa Istaltofa <istaltofafa@gmail.com>
2024-03-20 04:09:06 +0700

    02_namedConstructor

---
+++ my_oop/bin/01_namedConstructor.dart
@@ -0,0 +1,24 @@
+class Student{
+  String? name;
+  int? age;
+  int? rollNumber;
+
+  //default constructor
+  Student(){
+    print("This is a default constructor");
+  }
+  //named constructor
+  Student.namedConstructor(String name, int age, int rollNumber){
+    this.name = name;
+    this.age = age;
+    this.rollNumber = rollNumber;
+  }
+}
+
+void main(List<String> args) {
+  //disini student adalah object dari class Student
+  Student student = Student.namedConstructor("John", 20, 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number ${student.rollNumber}");
+}
