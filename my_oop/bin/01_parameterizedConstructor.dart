0be878d6f4975c17f9cfdc79ad57a92ca9397b1b
Marisa Istaltofa <istaltofafa@gmail.com>
2024-03-19 11:55:51 +0700

    01_parameterized.Constructor

---
+++ my_oop/bin/01_parameterizedConstructor.dart
@@ -0,0 +1,17 @@
+class Student{
+  String? name;
+  int? age;
+  int? rollNumber;
+
+  //konstruktor
+  Student(this.name, this.age, this.rollNumber);
+}
+
+void main() {
+  //student adalah object dari class
+  Student student = Student("John", 20, 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number: ${student.rollNumber}");
+
+}
