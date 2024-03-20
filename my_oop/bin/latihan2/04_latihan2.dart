634275dd58a636d0249b89ae73839426340fb064
Marisa Istaltofa <istaltofafa@gmail.com>
2024-03-20 15:30:14 +0700

    04_latihan2

---
+++ my_oop/bin/latihan2/04_latihan2.dart
@@ -0,0 +1,22 @@
+// Abstract class Bentuk
+abstract class Bentuk {
+  double hitungLuas();
+}
+
+// Class Lingkaran yang merupakan turunan dari Bentuk
+class Lingkaran extends Bentuk {
+  double jariJari;
+
+  Lingkaran(this.jariJari);
+
+  @override
+  double hitungLuas() {
+    return 3.14 * jariJari * jariJari;
+  }
+}
+
+void main() {
+  // Membuat objek Lingkaran
+  var lingkaran = Lingkaran(5);
+  print('Luas Lingkaran: ${lingkaran.hitungLuas()}');
+}
