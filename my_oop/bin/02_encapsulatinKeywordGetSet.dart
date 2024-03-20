23efa2d3da884ad09cd77ef416f38423f65955ac
Marisa Istaltofa <istaltofafa@gmail.com>
2024-03-20 15:03:30 +0700

    02_encapsulationKeywordGetSet

---
+++ my_oop/bin/02_encapsulatinKeywordGetSet.dart
@@ -0,0 +1,24 @@
+class Vehicle {
+  String _model = ''; // Inisialisasi _model dengan string kosong
+  int _year = 0; // Inisialisasi _year dengan 0
+
+  // Getter method
+  String get model => _model;
+
+  // Setter method
+  set model(String model) => _model = model;
+
+  // Getter method
+  int get year => _year;
+
+  // Setter method
+  set year(int year) => _year = year;
+}
+
+void main() {
+  var vehicle = Vehicle();
+  vehicle.model = "Toyota";
+  vehicle.year = 2019;
+  print(vehicle.model);
+  print(vehicle.year);
+}
